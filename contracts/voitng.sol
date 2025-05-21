b77e8efb27565308635be3808de47aa15300ff2d
praniil <pparajuli15@gmail.com>
2025-05-21 07:43:46 +0545

    feat: added a function to give right to vote to the voter

--- contracts/voitng.sol
+++ contracts/voitng.sol
@@ -32,4 +32,13 @@ contract Ballot{
             }));
         }        
     }
+
+    function giveRightToVote(address voter) external {
+        require(msg.sender == chairperson, "Only chairperson can give right to vote");
+        require(!voters[voter].voted, "The voter already voted");
+        require(voters[voter].weight == 0);
+        voters[voter].weight = 1;
+    }
+
+    
 }
